f4965dc3f53adb2b1740dbfba41d1b591c04c4ad
milena-sgoncalves <milena.goncalves@embrapii.org.br>
2025-03-19 15:49:34 -0300

    Inclusão da consulta da planilha 'plano_financeiro'.

--- consultas_clickhouse/main_consultas.py
+++ consultas_clickhouse/main_consultas.py
@@ -1,11 +1,11 @@
 import os
 import sys
-import shutil
 from dotenv import load_dotenv
 from scripts_public.apagar_arquivos_pasta import apagar_arquivos_pasta
 from scripts_public.main_registros_financeiros import main_registros_financeiros
 from scripts_public.main_anexo8 import main_anexo8
 from scripts_public.main_repasses import main_repasses
+from scripts_public.main_plano_financeiro import main_plano_financeiro
 from scripts_public.levar_arquivos_sharepoint import levar_arquivos_sharepoint
 import inspect
 
@@ -17,13 +17,14 @@ ARQUIVOS_BRUTOS = os.path.abspath(os.path.join(ROOT, '1_data_raw'))
 ARQUIVOS_PROCESSADOS = os.path.abspath(os.path.join(ROOT, '2_data_processed'))
 BACKUP = os.path.abspath(os.path.join(ROOT, 'backup'))
 
-def main(anexo8 = False, registros_financeiros = False, repasses = False, levar = False, an8_por_unidade = False, an8_por_projeto = False, an8_por_mes = False,
+def main(anexo8 = False, registros_financeiros = False, repasses = False, plano = False, levar = False, an8_por_unidade = False, an8_por_projeto = False, an8_por_mes = False,
          an8_ano_especifico = None, an8_mes_especifico = None, an8_tirar_desqualificados = False):
     """
     Função principal que chama as funções de processamento dos arquivos brutos.
         anexo8: Se True, processa o Anexo 8.
         registros_financeiros: Se True, processa os Registros Financeiros.
         repasses: Se True, processa os Repasses.
+        plano: Se True, processa os Planos Financeiros.
         levar: Se True, leva os arquivos processados para o Sharepoint.
         an8_por_unidade: Se True, processa o Anexo 8 por Unidade.
         an8_por_projeto: Se True, processa o Anexo 8 por Projeto.
@@ -51,6 +52,9 @@ def main(anexo8 = False, registros_financeiros = False, repasses = False, levar 
         if repasses:
             main_repasses()
 
+        if plano:
+            main_plano_financeiro()
+
         if levar:
             print("Levando arquivos processados para o Sharepoint")
             levar_arquivos_sharepoint()
@@ -60,5 +64,5 @@ def main(anexo8 = False, registros_financeiros = False, repasses = False, levar 
         print(f"🔴 Erro: {e}")
 
 if __name__ == "__main__":
-    main(anexo8=True, registros_financeiros=True, repasses=True, levar=True,
-         an8_por_unidade=True, an8_por_projeto=True, an8_por_mes=True)
+    main(anexo8=True, registros_financeiros=True, repasses=True, plano=True,
+         levar=True, an8_por_unidade=True, an8_por_projeto=True, an8_por_mes=True)
